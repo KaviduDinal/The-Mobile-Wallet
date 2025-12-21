eb9b607d8ceab14c96e6f0f8e559cdd8ef33630c
KaviduDinal <dinalkavidu5@gmail.com>
2025-12-22 01:42:21 +0530

    Start to set up the mobile side

---
+++ mobile/app/index.jsx
@@ -0,0 +1,26 @@
+import { Image, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+
+
+      }}
+    >
+      <Text style={{ color: "blue" }}>Edit app/index.tsx to edit this screen.123</Text>
+      <Link href={"/about"}>About</Link>
+      <View>
+        <Text>
+          Hello
+        </Text>
+      </View>
+
+    </View>
+  );
+}
+
