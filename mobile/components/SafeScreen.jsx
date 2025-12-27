dd6308b10c638318d6516bdb66ca8849707bad28
KaviduDinal <dinalkavidu5@gmail.com>
2025-12-27 23:08:27 +0530

    before the setup Authentication  all are set

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from 'react-native';
 
-const SafeScreen = () => {
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { COLORS } from '@/constants/colors.js';
+
+
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets()
+
   return (
-    <View>
-      <Text>SafeScreen</Text>
+    <View style={{ paddingTop: insets.top, flex: 1,paddingBottom:insets.bottom , backgroundColor: COLORS.background }}>
+      {children}
     </View>
-  )
+  );
 }
 
 export default SafeScreen
